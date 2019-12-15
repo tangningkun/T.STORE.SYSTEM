c5ac66b2ca6cff4f882f1770b5e6e574d1be04c9
TangNingKun <1209229446@qq.com>
2019-12-15 21:25:58 +0800

    init

---
+++ T.STORE.SYSTEM.Project/T.STORE.SYSTEM.EntityFrameworkCore/StoreDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T.STORE.SYSTEM.EntityFrameworkCore
+{
+    public class StoreDbContext : DbContext
+    {
+        public StoreDbContext(DbContextOptions<StoreDbContext> options) : base(options)
+        {
+        }
+
+        //自定义DbContext实体属性名与数据库表对应名称（默认 表名与属性名对应是 User与Users）
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+    }
+}
---
+++ T.STORE.SYSTEM.Project/T.STORE.SYSTEM.Web/Common/AppConfigurtaion.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace T.STORE.SYSTEM.Web.Common
+{
+    public class AppConfigurtaionServices
+    {
+        /*public static IConfiguration Configuration { get; set; }
+        static AppConfigurtaionServices()
+        {
+            //ReloadOnChange = true 当appsettings.json被修改时重新加载            
+            Configuration = new ConfigurationBuilder()
+            .Add(new JsonConfigurationSource { Path = "appsettings.json", ReloadOnChange = true })
+            .Build();
+        }*/
+        private readonly IConfiguration _configuration;
+        public AppConfigurtaionServices(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public string AppConfigurtaionValue(string name)
+        {
+            return _configuration[name.Trim()].ToString();
+        }
+    }
+}
